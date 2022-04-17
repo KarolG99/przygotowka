7929bc24551af02bfab21dfa34b49eefba72facf
Karol Gucwa <karolus2212@gmail.com>
2022-04-17 10:32:38 +0200

    changed api link

--- src/apiurl.ts
+++ src/apiurl.ts
@@ -1,2 +1,2 @@
-// export const URL = "https://przygotowka.herokuapp.com";
-export const URL = "http://localhost:8000";
+export const URL = "https://przygotowka.herokuapp.com";
+// export const URL = "http://localhost:8000";
